5b92086421250e68fa8733b6f29710aabb247e2f
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-08 18:53:57 -0600

    Had to fix the functionality of the hit state

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -7,53 +7,53 @@ import './App.css';
 
 function App() {
 
-    const [strikes, setStrikes] = useState(0)
-    const [balls, setBalls] = useState(0)
-    const [fouls, setFouls] = useState(0)
-    const [hits, setHits] = useState(0)
+  const [strikes, setStrikes] = useState(0)
+  const [balls, setBalls] = useState(0)
+  const [fouls, setFouls] = useState(0)
+  const [hits, setHits] = useState(0)
 
 
-    const actions = {
-        strike: () => {
-            setStrikes(strikes +1);
-            if (strikes === 2){
-              return setStrikes(0), setBalls(0);
-            }
-        },
+  const actions = {
+    strike: () => {
+      setStrikes(strikes + 1);
+      if (strikes === 2) {
+        return (setStrikes(0), setBalls(0));
+      }
+    },
 
-        ball: () => {
-          setBalls(balls + 1);
-          if (balls === 3) {
-            return setBalls(0), setStrikes(0), setHits(hits + 1);
-          }
-        },
+    ball: () => {
+      setBalls(balls + 1);
+      if (balls === 3) {
+        return (setBalls(0), setStrikes(0), setHits(hits + 1));
+      }
+    },
 
-        foul : () => {
-          if( strikes <= 1) {
-            return setStrikes(strikes + 1)
-          }
-        },
+    foul: () => {
+      if (strikes <= 2) {
+        return setStrikes(strikes + 1)
+      }
+    },
 
-        score: () => {
-          if(setHits(hits + 1)) {
-            return setBalls(0), setStrikes(0), setFouls(0);
-          }
-        }
+    score: () => {
+      setHits(hits + 1);
+      return (setBalls(0), setStrikes(0));
+
     }
+  }
 
-    
+
 
   return (
     <div className="App">
       <h1>Play Ball!</h1>
-      <Display 
-      strike={strikes}
-      ball={balls}
-      foul={fouls}
-      score={hits}
+      <Display
+        strike={strikes}
+        ball={balls}
+        foul={fouls}
+        score={hits}
       />
-      <Dashboard 
-                play={actions}
+      <Dashboard
+        play={actions}
       />
     </div>
   );
